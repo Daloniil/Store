52ff8889c83553e6e05f5268ff8b0c6cbfb9f968
Daloniil <62095873+Daloniil@users.noreply.github.com>
2021-08-04 04:47:54 +0300

    Finally version(1.1.3)

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './Redux/redux-store'
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import ReactDom from 'react-dom';
 import {Provider} from "react-redux";
 
 
 ReactDom.render(
-    < BrowserRouter basename={process.env.PUBLIC_URL}>
+    < HashRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>, document.getElementById('root')
+    </HashRouter>, document.getElementById('root')
 );
 
 
