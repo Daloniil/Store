ff4186fa798e9e3eae0212105d92211b94049745
Daloniil <62095873+Daloniil@users.noreply.github.com>
2022-02-09 00:51:41 +0200

    create(Assets)

--- src/Components/Profile/CenterHeader/CenterHeader.tsx
+++ src/Components/Profile/CenterHeader/CenterHeader.tsx
@@ -1,40 +1,70 @@
-import s from './StyleCenterHeader/CenterHeader.module.css'
-import {NavLink} from "react-router-dom";
+import s from "./StyleCenterHeader/CenterHeader.module.css";
+import { NavLink } from "react-router-dom";
 import React from "react";
 
-
 const CenterHeader: React.FC = () => {
-
-    return (
-        <div className={s.center_header}>
-            <div className={s.center_header_container}>
-                <div className={s.center_header_container_text}>
-                    <span>Смачне й швидке</span>
-                    <h2>Меню доставки</h2>
-                    <p>Зробіть швидке замовлення, якщо Ви впевненні у своєму смаку та <br/> побажаннях. Якщо ні – для
-                        Вас є повне меню зі зручними фільтрами <br/> для пошуку у розділі <a href="#">Меню.</a></p>
-                </div>
-            </div>
-
-
-            <div className={s.menu}>
-                <div className={s.pizza}>
-                    <span><NavLink to="/profile/pizza" className={s.piza}
-                                   activeClassName={s.ac}>Пица</NavLink></span>
-                </div>
-                <div className={s.sneks}>
-                    <span><NavLink to="/profile/sneks" className={s.snek} activeClassName={s.ac}>Снеки</NavLink></span>
-                </div>
-                <div className={s.drink}>
-                    <span><NavLink to="/profile/drink" className={s.drin} activeClassName={s.ac}>Напої</NavLink></span>
-                </div>
-                <div className={s.soys}>
-                    <span><NavLink to="/profile/sous" className={s.soy} activeClassName={s.ac}>Соуси</NavLink></span>
-                </div>
-            </div>
+  return (
+    <div className={s.center_header}>
+      <div className={s.center_header_container}>
+        <div className={s.center_header_container_text}>
+          <span>Смачне й швидке</span>
+          <h2>Меню доставки</h2>
+          <p>
+            Зробіть швидке замовлення, якщо Ви впевненні у своєму смаку та
+            <br /> побажаннях. Якщо ні – для Вас є повне меню зі зручними
+            фільтрами <br /> для пошуку у розділі <a href="#">Меню.</a>
+          </p>
         </div>
-    )
+      </div>
 
-}
+      <div className={s.menu}>
+        <div className={s.pizza}>
+          <span>
+            <NavLink
+              to="/profile/pizza"
+              className={s.piza}
+              activeClassName={s.ac}
+            >
+              Бакалія
+            </NavLink>
+          </span>
+        </div>
+        <div className={s.sneks}>
+          <span>
+            <NavLink
+              to="/profile/sneks"
+              className={s.snek}
+              activeClassName={s.ac}
+            >
+              Кондитерскі вироби
+            </NavLink>
+          </span>
+        </div>
+        <div className={s.drink}>
+          <span>
+            <NavLink
+              to="/profile/drink"
+              className={s.drin}
+              activeClassName={s.ac}
+            >
+              Напої
+            </NavLink>
+          </span>
+        </div>
+        <div className={s.soys}>
+          <span>
+            <NavLink
+              to="/profile/sous"
+              className={s.soy}
+              activeClassName={s.ac}
+            >
+              Чай/Кофе
+            </NavLink>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CenterHeader;
